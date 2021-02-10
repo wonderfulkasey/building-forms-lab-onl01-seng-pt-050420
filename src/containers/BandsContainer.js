a83816a7bc8b277cc6acb930b6b6023b9711089b
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-10 19:41:46 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-
+import BandInput from '../components/BandInput'
+import {addBand} from '../actions/bands'
+import { connect } from 'react-redux'
+import Bands from '../components/Bands'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        
       </div>
     )
   }
